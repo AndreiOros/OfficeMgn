631034305aa6aee9c461af3c60e1e1624fabd9b6
AndreiOros <andrei.oros111@gmail.com>
2018-07-23 10:31:56 +0300

    initial
    initial

---
+++ AccesaEmployee/Employee.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace AccesaEmployee
+{
+	public abstract class Employee
+	{
+		private string _name;
+		private EmployeePosition _position;
+		private float _capacity;//max number of hours per day
+		private List<string> h1=new List<string>();
+        private string hobbie;
+
+        public string Name => _name;
+		public EmployeePosition Position => _position;
+		public float Capacity => _capacity;
+		public List<string> Hobbies => h1;
+
+		protected Employee(string name, EmployeePosition position, float capacity)
+		{
+			_name = name;
+			_position = position;
+			_capacity = capacity;
+		}
+
+		public virtual void DisplayInfo()
+		{
+			var sb= new StringBuilder();
+			h1.ForEach(x=>sb.Append(x+" "));
+			Console.WriteLine($"{_name} ocupa pozitia {_position} si e angajat cu {_capacity} ore pe zi. Lui ii place {sb.ToString()}");
+		}
+
+        public Employee(XmlReader r) { ReadXML(r); }
+
+        public virtual void ReadXML(XmlReader r)
+        {
+            r.ReadStartElement();
+            _name = r.ReadElementContentAsString("Name", " ");
+            _capacity = r.ReadElementContentAsFloat("capacity", "");
+            //_position = r.ReadElementContentAsObject("Position: ", "");
+            foreach (string h in Hobbies)
+            {
+                r.ReadElementContentAsString("Hobbies:", "");
+            }
+            r.ReadEndElement();
+        }
+
+        public virtual void WriteXML(XmlWriter w)
+        {
+            w.WriteElementString("Name", _name.Trim());
+            w.WriteElementString("Capacity", XmlConvert.ToString(Capacity));
+           // w.WriteElementString("Position", XmlConvert.ToString(Position));
+            foreach (string i in Hobbies)
+            {
+                w.WriteElementString("Hobby", i);
+            }
+        }
+    }
+}
---
+++ AccesaEmployee/Intern.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace AccesaEmployee
+{
+	public class Intern:Employee
+	{
+		private string _universityName;
+		private int _yearOfStudy;
+		private EmployeePosition _targetPosition;
+
+		public string UniversityName => _universityName;
+		public int YearOfStudy => _yearOfStudy;
+		public EmployeePosition TargetPosition => _targetPosition;
+		public Intern(string name, float capacity) 
+			: base(name, EmployeePosition.Intern, capacity)
+		{}
+
+        public override void ReadXML(XmlReader r)
+        {
+            base.ReadXML(r);
+            _universityName = r.ReadElementContentAsString("UniversityName:", "");
+            _yearOfStudy = r.ReadElementContentAsInt("YearOfStudy", "");
+        }
+
+        public override void WriteXML(XmlWriter w)
+        {
+            base.WriteXML(w);
+            w.WriteElementString("UniversityName", _universityName);
+            w.WriteElementString("YearOfStudy", _yearOfStudy.ToString());
+        }
+
+    }
+}
